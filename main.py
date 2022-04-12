5fa535a74dc87ffcd12fee29128357aa460d77bd
Jessika Risberg <33091640+JessikaRisberg@users.noreply.github.com>
2022-04-12 09:44:04 +0200

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Enter your height (in cm): ")
+    height = input()
+    weight = input("Enter your weight (in KG): ")
+
+    print(f'Your is {height} and your weight are {weight}')
+
+
+if __name__ == '__main__':
+    main()
